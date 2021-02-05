bc5f1c2674f17553b3f769c04e39ee1b732c4663
MUHAMMED KOÇAK <muhammed_kocak26@hotmail.com>
2021-02-05 23:45:17 +0300

    Business; Concrete: ColorManager,BrandManager / Abstract: IColorService, IBrandService

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,11 +10,25 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             CarManager carManager = new CarManager(new EfCarDal());
-
             foreach (var product in carManager.GetAll())
             {
                 Console.WriteLine(product.ModelYear);
             }
+            Console.WriteLine("--CAR--");
+
+            BrandManager brandManager = new BrandManager(new EfBrandDal());
+            foreach (var brand in brandManager.GetAll())
+            {
+                Console.WriteLine(brand.BrandName);
+            }
+            Console.WriteLine("--BRAND--");
+
+            ColorManager colorManager = new ColorManager(new EfColorDal());
+            foreach (var color in colorManager.GetAll())
+            {
+                Console.WriteLine(color.ColorName);
+            }
+            Console.WriteLine("--COLOR--");
         }
     }
 }
